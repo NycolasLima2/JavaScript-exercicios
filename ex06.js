80615448bc2235202571e54e8ffc4ea3ba2aca52
NycolasLima2 <nycolas.lima@edu.senai.br>
2026-04-22 16:40:10 -0300

    Conclusão do exercicio 6: Troca de valores

---
+++ ex06.js
@@ -0,0 +1,8 @@
+const prompt = require('prompt-sync')();
+
+console.log("----- Troca de Valores  -----");
+let logA = (prompt("insira o valor A: "));
+let logB = (prompt("insira o valor B: "));
+
+console.log(`valor A = ${logB}`);
+console.log(`valor B = ${logA}`);

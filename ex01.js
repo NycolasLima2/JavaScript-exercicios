07c7ab27a8bf75c4047be2747d7dac64cc9ba8ee
NycolasLima2 <nycolas.lima@edu.senai.br>
2026-04-22 14:39:46 -0300

    Conclusão do exercicio 1 da lista: Conversor de temperatura

--- ex01.js
+++ ex01.js
@@ -1,6 +1,10 @@
 // Importando o módulo que você via terminal
 const prompt = require('prompt-sync')();
 
+console.log(`----- Conversor de Temperatura -----`);
 
-output(`aaaaaaaaaaaaa`);
+let CG = (prompt("insira o valor, em graus Celcius, uma temperatura: "));
 
+let Conversor = CG * 9/5 + 32; 
+
+console.log(`O valor da temperatra em F (Fahrenheit): ${Conversor}`);

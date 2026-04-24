e19b421c5ef7b43c301033fe2b2ae718738739e0
NycolasLima2 <nycolas.lima@edu.senai.br>
2026-04-24 08:38:22 -0300

    Conclusão da programação do exercicio 9

---
+++ ex09.js
@@ -0,0 +1,7 @@
+const prompt = require('prompt-sync')();
+
+console.log("----- Cáuculo do quadrado de um numero inteiro -----");
+
+var V1 = (prompt("insira o valor para ser elevado ao quadrado: "));
+let Re = V1 * V1 ;
+console.log(`o Resutado é : ${Re}`);

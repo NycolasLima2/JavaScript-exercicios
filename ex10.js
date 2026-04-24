651145c34711b69734e8af30e23ae3e14b1ae802
NycolasLima2 <nycolas.lima@edu.senai.br>
2026-04-24 08:51:12 -0300

    Conclusão da programação do exercicio 10

---
+++ ex10.js
@@ -0,0 +1,13 @@
+const prompt = require('prompt-sync')();
+
+console.log("----- Cáuculo da diferença entre 2 numeros inteiros -----");
+
+var V1 = (prompt("insira um valor inteiro: "));
+let V2 = (prompt("insira o sehundo valor: "));
+
+var Re = (V1) - V2;
+
+console.log(`O Valor final é ${Re}`);
+
+
+

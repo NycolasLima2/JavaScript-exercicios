afb6e147f812cf2b4511ff7888c7edede2483f41
NycolasLima2 <nycolas.lima@edu.senai.br>
2026-04-24 15:57:50 -0300

    conclusão do exercicio 19

--- ex19.js
+++ ex19.js
@@ -1,9 +1,16 @@
 const prompt = require('prompt-sync')();
 
-console.log("----- Cáuculo de Volume de uma caixa -----");
+console.log("----- Cáuculo de todas as operações basicas com 2 numeros-----");
 
-let Raio = Number(prompt("Insira um valor: "));
+let V1 = Number(prompt("Insira um valor: "));
+let V2 = Number(prompt("Insira um valor: "));
 
-let R = Math.PI * (Raio * Raio);
+let R = V1 + V2;
+let R2 = V1 - V2;
+let R3 = V1 * V2;
+let R4 = V1 / V2;
 
-console.log(`o Resultado foi ${Math.round(R)}`);
+console.log(`o Resultado foi ${(R)}`);
+console.log(`o Resultado foi ${(R2)}`);
+console.log(`o Resultado foi ${Math.round(R3)}`);
+console.log(`o Resultado foi ${Math.round(R4)}`);

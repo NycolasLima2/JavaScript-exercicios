53da88ab4bcc4b1bcf65de7e8a4d4b6d3a5d962b
NycolasLima2 <nycolas.lima@edu.senai.br>
2026-04-24 16:34:41 -0300

    Conclusão do exercicio 22

---
+++ ex22.js
@@ -0,0 +1,10 @@
+const prompt = require('prompt-sync')();
+
+console.log("----- Cáuculo de potencia----");
+
+let Raio = Number(prompt("Insira o valor do raio: "));
+
+
+let R = (4/3)* Math.PI* Math.pow(Raio,3);
+
+console.log(`o Resultado foi ${(R)}`);

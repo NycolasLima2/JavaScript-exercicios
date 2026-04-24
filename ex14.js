5a4b68696cc7d3b50a646365c652bc6b718dcab5
NycolasLima2 <nycolas.lima@edu.senai.br>
2026-04-24 09:17:40 -0300

    conclusão da programação do exercicio 14

---
+++ ex14.js
@@ -0,0 +1,10 @@
+const prompt = require('prompt-sync')();
+
+console.log("----- Cáuculo de Volume de uma caixa -----");
+
+let V1 = (prompt("Insira um valor: "));
+let V2 = (prompt("Insira um valor: "));
+let V3 = (prompt("Insira um valor: "));
+
+let R = ((V1 * 1) + (V2 * 1) + (V3 * 1)) * ((V1 * 1) + (V2 * 1) + (V3 * 1));
+console.log(`o Resultado foi ${R}`);

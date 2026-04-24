f5859169d8923928e730ce34c1b5d23fadfad2e7
NycolasLima2 <nycolas.lima@edu.senai.br>
2026-04-24 17:10:06 -0300

    Conclusão do exercicio 24

---
+++ ex24.js
@@ -0,0 +1,10 @@
+const prompt = require('prompt-sync')();
+
+console.log("----- Cáuculo de potencia----");
+
+let Raio = Number(prompt("Insira um valor: "));
+let ELV = Number(prompt("insira o expoente: "))
+
+let R = Math.pow(Raio,(1/ELV));
+
+console.log(`o Resultado foi ${(R)}`);

809b5a07e215d4d1eda77dde4b17168f81f850f1
NycolasLima2 <nycolas.lima@edu.senai.br>
2026-04-29 08:54:11 -0300

    Conclusão do exercicio 26: Quadrado da divisão

---
+++ Estrutura-sequencial/ex26.js
@@ -0,0 +1,13 @@
+const prompt = require('prompt-sync')();
+
+console.log("----- caucolo do quadrado da divisão de dois numeros ----");
+
+let N= Number(prompt("um numero natural: "));
+let N1= Number(prompt("segundo numero natural: "));
+
+
+let Na = N / N1;
+let R = Math.pow(Na, 2);
+
+
+console.log(`o resultado deu em ${R}`);

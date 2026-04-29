7f45e070a7e7c01fc26ccb87b1f446e22fb65db0
NycolasLima2 <nycolas.lima@edu.senai.br>
2026-04-29 16:21:44 -0300

    erro D-a

---
+++ E2strutura-De-Decisão/ex38.js
@@ -0,0 +1,18 @@
+const prompt = require('prompt-sync')();
+
+console.log("--- valor menor ou igual a 3 ");
+
+const V1 = Number(prompt("insira um valor: "));
+
+if(isNaN(V1)){
+    console.log("erro: não tem apenas numeros");
+}else{
+
+if(V1 <= 3){
+   console.log(`${V1}`); 
+}else{
+    console.log("      ");
+}
+
+
+}
---
+++ E2strutura-De-Decisão/ex39.js
@@ -0,0 +1,17 @@
+const prompt = require('prompt-sync')();
+
+console.log("--- 3 e 5 ---");
+
+const V1 = Number(prompt("insira um numero"));
+
+if(isNaN(V1)){
+    console.log("erro de execução. insira um numero");
+}else{
+
+if(V1 / 3 === 0 && V1 / 5 === 0){
+    console.log(`${V1}`);
+}
+
+
+
+}

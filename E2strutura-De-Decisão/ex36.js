62455b19490f9ad65614524f9e7ad3162a6e250e
NycolasLima2 <nycolas.lima@edu.senai.br>
2026-04-29 15:29:59 -0300

    Conclusão 36

---
+++ E2strutura-De-Decisão/ex36.js
@@ -0,0 +1,15 @@
+const prompt = require('prompt-sync')();
+
+console.log("--- Maior e Menor ---");
+const V1 = Number(prompt("insira um numero: "));
+
+if(isNaN(V1)){
+console.log("Dica: use numeros");
+}else{
+
+if(V1 % 2 === 0){
+  console.log("Par");  
+}else{
+    console.log("Impar");
+}
+}

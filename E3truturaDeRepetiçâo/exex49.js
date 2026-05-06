839c8cae35e5a224d576302f0eea402f649b98d3
NycolasLima2 <nycolas.lima@edu.senai.br>
2026-05-06 16:48:01 -0300

    correçao

--- E3truturaDeRepetiçâo/exex49.js
+++ E3truturaDeRepetiçâo/exex49.js
@@ -5,7 +5,6 @@ console.log("--- Potencia de uma base Elevada a um expoente---");
 let base = Number(prompt("insira a base: "));
 let expoente = Number(prompt("insira o expoente: "));
 
-// Iniciamos com 1, pois todo número elevado a 0 é 1
 let potencia = 1;
 
 for (let n = 1; n <= expoente; n++) {
